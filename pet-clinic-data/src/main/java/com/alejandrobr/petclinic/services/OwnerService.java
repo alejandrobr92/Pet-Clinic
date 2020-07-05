46ccaa52d9a67008f8df74da9cd2556a35b56e85
alejandro <reyesbautista.contact@gmail.com>
2020-07-04 19:19:12 -0500

    Implemented map based interfaces. 62.

--- pet-clinic-data/src/main/java/com/alejandrobr/petclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/alejandrobr/petclinic/services/OwnerService.java
@@ -2,8 +2,6 @@ package com.alejandrobr.petclinic.services;
 
 import com.alejandrobr.petclinic.model.Owner;
 
-import java.util.Set;
-
 public interface OwnerService extends CrudService<Owner, Long> {
     Owner findByLastName(String lastName);
 
